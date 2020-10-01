ed057f15800e56c1133b83989f3d3c25be857a23
VishalRishi23 <62276270+VishalRishi23@users.noreply.github.com>
2020-10-01 22:10:32 +0530

    Update Image Classifier 1.py

--- Image Classifier 1.py
+++ Image Classifier 1.py
@@ -3,7 +3,7 @@
 
 # In[15]:
 
-
+#Import the libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
@@ -58,14 +58,14 @@ ytrain,yval=ytrain[:4000],ytrain[4000:]
 
 # In[19]:
 
-
+#Dealing with class imbalance
 from sklearn.utils import class_weight
 a=class_weight.compute_class_weight('balanced',np.array([0.,1.,2.,3.]),ytrain)
 
 
 # In[5]:
 
-
+#Data augmentation
 from keras.preprocessing.image import ImageDataGenerator
 datagen=ImageDataGenerator(rotation_range=40,shear_range=0.2,zoom_range=0.2,horizontal_flip=True,brightness_range=(0.5,1.5),width_shift_range=0.1,height_shift_range=0.1)
 datagen.fit(Xtrain)
@@ -73,7 +73,7 @@ datagen.fit(Xtrain)
 
 # In[23]:
 
-
+#Custom ResNet 34 network
 from functools import partial
 DefaultConv2D = partial(keras.layers.Conv2D, kernel_size=3, strides=1,padding="SAME", use_bias=False)
 class ResidualUnit(keras.layers.Layer):
@@ -132,7 +132,7 @@ model.compile(loss='sparse_categorical_crossentropy',optimizer='Adam',metrics=['
 
 # In[10]:
 
-
+#Learning rate range test
 from keras_lr_finder import LRFinder
 lr_finder=LRFinder(model)
 outputs=lr_finder.find_generator(datagen.flow(Xtrain,ytrain,batch_size=32,shuffle=True),0.00001,10,validation_data=[Xval,yval],class_weight=a,epochs=4)
@@ -142,7 +142,7 @@ lr_finder.plot_loss()
 # In[11]:
 
 
-print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.25
+print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.25 (dropout)
 
 
 # In[14]:
@@ -156,7 +156,7 @@ lr_finder.plot_loss()
 # In[15]:
 
 
-print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.0
+print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.0 (No dropout)
 
 
 # In[18]:
@@ -170,7 +170,7 @@ lr_finder.plot_loss()
 # In[19]:
 
 
-print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.30
+print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.30 (Dropout)
 
 
 # In[22]:
@@ -184,7 +184,7 @@ lr_finder.plot_loss()
 # In[23]:
 
 
-print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.25, wd=0.1
+print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.25, wd=0.1 (Dropout and weight decay)
 
 
 # In[26]:
@@ -198,7 +198,7 @@ lr_finder.plot_loss()
 # In[27]:
 
 
-print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.25, wd=0.01
+print(np.amin(lr_finder.losses),lr_finder.lrs[np.argmin(lr_finder.losses)]) # p=0.25, wd=0.01 (Dropout and weight decay)
 
 
 # In[ ]:
@@ -251,7 +251,7 @@ history = model.fit(Xtrain, ytrain,batch_size=batch_size, epochs=n_epochs, valid
 # In[ ]:
 
 
-history = model.fit_generator(datagen.flow(Xtrain, ytrain,batch_size=32), epochs=100,initial_epoch=80, validation_data=[Xval,yval],class_weight=a)          
+history = model.fit_generator(datagen.flow(Xtrain, ytrain,batch_size=32), epochs=100,initial_epoch=0, validation_data=[Xval,yval],class_weight=a)          
 
 
 # In[12]:
